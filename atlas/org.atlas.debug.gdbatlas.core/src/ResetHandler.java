7d618a0b0c6996c257b896261578cdb43d6a86dd
Prasanna Bhat <prasanna.bhat@in.bosch.com>
2014-04-22 17:53:43 +0530

    Add sample reset menu item

---
+++ atlas/org.atlas.debug.gdbatlas.core/src/ResetHandler.java
@@ -0,0 +1,16 @@
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+
+public class ResetHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+//		HandlerUtil.getActiveWorkbenchWindow(event).close();
+		return null;
+	}
+
+
+}
